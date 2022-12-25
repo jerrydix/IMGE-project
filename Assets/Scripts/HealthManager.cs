24b87e59b1cfc724eb22ac172cb067766b5a108e
J3rry <jerrygamerhd@gmail.com>
2022-12-25 23:17:39 +0100

    added HealthManager

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public enum DamageType
+    {
+        Armor,
+        Normal
+    }
+    public static HealthManager Instance { get; set; }
+    public int health;
+    public int armor;
+    public bool dead;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        health = 100;
+        armor = 100;
+        dead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int amount, DamageType type)
+    {
+        switch (type)
+        {
+            case DamageType.Armor:
+            {
+                armor -= amount;
+                if (armor < 0)
+                {
+                    health += armor;
+                    armor = 0;
+                }
+                break;
+            }
+            case DamageType.Normal:
+            {
+                health -= amount;
+                break;
+            }
+        }
+        if (health > 0) return;
+        dead = true;
+        health = 0;
+    }
+
+    public void Heal(int amount, DamageType type)
+    {
+        switch (type)
+        {
+            case DamageType.Armor:
+            {
+                armor += amount;
+                if (armor >= 100) armor = 100;
+                break;
+            }
+            case DamageType.Normal:
+            {
+                health += amount;
+                if (health >= 100) health = 100;
+                break;
+            }
+        }
+    }
+}
