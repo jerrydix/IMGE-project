8f8f0f3995f0fb58024bcac2b4c1f2384c3f0387
J3rry <ge58ler@mytum.de>
2023-01-13 13:31:12 +0100

    added radio for wayfinding

---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalAudio : MonoBehaviour
+{
+
+    private AudioSource _source;
+    [SerializeField] private AudioClip audioTrack;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+        _source.clip = audioTrack;
+        _source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
