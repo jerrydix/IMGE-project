ac08558b4ccfdb5eb65524ac8da2388adf05f2c3
SillesianGuy <jakub.jan.luczak@gmail.com>
2023-01-18 14:02:50 +0100

    Options Menu: Slider not working

---
+++ Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    private Slider volumeSlider;
+    private Toggle fullscreenToggle;
+    [SerializeField] private AudioMixer mixer;
+
+    public void SetVolume(float volume)
+    {
+        mixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+        GameManagement.Instance.ChangeVolume(Mathf.Log10(volume) * 20);
+    }
+
+    public void SetFullscreen(bool fullscreened)
+    {
+        Screen.fullScreen = fullscreened;
+    }
+}
