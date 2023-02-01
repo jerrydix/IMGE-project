a1cc1864e4affff7b4407f73692de395a48125b6
J3rry <ge58ler@mytum.de>
2023-02-01 13:42:10 +0100

    fix

--- Assets/Scripts/MainMenu/LevelSelection.cs
+++ Assets/Scripts/MainMenu/LevelSelection.cs
@@ -5,29 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
+    [SerializeField] private LevelChanger changer;
     public void Load0()
     {
-        GameObject.Find("LevelChanger").GetComponent<LevelChanger>().Fade(false, 0);
+        changer.Fade(false, 0);
     }
     
     public void Load1()
     {
-        GameObject.Find("LevelChanger").GetComponent<LevelChanger>().Fade(false, 1);
+        changer.Fade(false, 1);
     }
     
     public void Load2()
     {
-        GameObject.Find("LevelChanger").GetComponent<LevelChanger>().Fade(false, 2);
+        changer.Fade(false, 2);
     }
     
     public void Load3()
     {
-        GameObject.Find("LevelChanger").GetComponent<LevelChanger>().Fade(false, 3);
+        changer.Fade(false, 3);
     }
     
     public void Load4()
     {
-        GameObject.Find("LevelChanger").GetComponent<LevelChanger>().Fade(false, 4);
+        changer.Fade(false, 4);
     }
     
     /*private void LoadLevel(int sceneIndex)
